61404d52558fad3ce4431edd49d279795004695b
Adetokunbo Ige <aige@tempo.io>
2024-11-06 19:37:39 -0800

    chore: improvements
    Signed-off-by: Adetokunbo Ige <aige@tempo.io>

--- test_selenium.py
+++ test_selenium.py
@@ -1,12 +1,4 @@
-import os
-import time
-from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.edge.options import Options as EdgeOptions
-
+# In your test_selenium.py file
 def get_driver():
     browser = os.getenv("BROWSER", "chrome").lower()
 
@@ -29,7 +21,7 @@ def get_driver():
     for attempt in range(5):
         try:
             driver = webdriver.Remote(
-                command_executor='http://selenium:4444/wd/hub',
+                command_executor='http://localhost:4444/wd/hub',
                 options=options
             )
             break
@@ -41,14 +33,3 @@ def get_driver():
         raise RuntimeError("Failed to connect to the Selenium server after multiple attempts.")
     
     return driver
-
-def test_google_title():
-    driver = get_driver()
-    try:
-        driver.get("https://www.google.com")
-        assert "Google" in driver.title
-    finally:
-        driver.quit()
-
-if __name__ == "__main__":
-    test_google_title()
