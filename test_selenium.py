130d0be744fc5ac88e71e03e1962c61af67e6477
Adetokunbo Ige <aige@tempo.io>
2024-11-06 20:23:11 -0800

    chore: improvements
    Signed-off-by: Adetokunbo Ige <aige@tempo.io>

--- test_selenium.py
+++ test_selenium.py
@@ -1,8 +1,10 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 def test_open_google():
-    driver = webdriver.Chrome()
+    # Use WebDriver Manager to get the correct driver
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://www.google.com")
     assert "Google" in driver.title
     driver.quit()
